5c8e8a8d3de24750424bd7a3d1de5484cb091674
sociablerobot10 <19kam.sha@gmail.com>
2024-07-12 11:45:28 -0400

    add share feature

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+/* variables */
 let firstCard = document.querySelector(".card-appear");
 let secondCard = document.querySelector(".card-invite");
 let aboutCard = document.querySelector(".about-cont");
 let mapCard = document.querySelector(".map-cont");
+let socialsCard = document.querySelector(".socials");
 let openBtn = document.querySelector(".open-btn");
 let moreBtn = document.querySelector(".more-btn");
 let mapBtn = document.querySelector(".map-btn");
+let socialsBtn = document.querySelector(".socials-btn");
 
+// animate card leaving and back card entering on click
 openBtn.addEventListener("click", () => {
   firstCard.classList.add("fade-out");
   setTimeout(function () {
@@ -15,28 +19,32 @@ openBtn.addEventListener("click", () => {
       "animate__zoomOut"
     );
 
-    // firstCard.classList.add("card-disappear");
+    // less aburpt exit
     setTimeout(function () {
       firstCard.className = "card-disappear";
     }, 500);
-
+    // Delay second card arrival
     setTimeout(function () {
-      /** get second card to pulse or do something when it shows up */
       secondCard.classList.add("animate__zoomInLeft");
       secondCard.classList.remove("card-disappear");
     }, 500);
   });
 });
 
+// about section will will slide in on click
 moreBtn.addEventListener("click", () => {
   aboutCard.classList.remove("card-disappear");
   aboutCard.scrollIntoView({ behavior: "smooth" });
   aboutCard.classList.add("animate__animated", "animate__slideInLeft");
 });
-
+// map section will will slide in on click
 mapBtn.addEventListener("click", () => {
   mapCard.classList.remove("card-disappear");
   mapCard.scrollIntoView({ behavior: "smooth" });
   mapCard.classList.add("animate__animated", "animate__slideInLeft");
-  // aboutCard.classList.add("animate__animated", "animate__backInUp");
+});
+
+/* Share with your friends functionality */
+socialsBtn.addEventListener("click", () => {
+  socialsCard.classList.remove("card-disappear");
 });
